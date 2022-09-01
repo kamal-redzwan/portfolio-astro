9763b8ffb2f0036b0404a5f276e05f7b61497a25
kamal-redzwan <kmlrdzwn.dev@gmail.com>
2022-09-01 23:26:42 +0800

    First Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    safelist: ["w-64", "w-1/2", "rounded-l-lg", "rounded-r-lg", "bg-gray-200", "grid-cols-4", "grid-cols-7", "h-6", "leading-6", "h-9", "leading-9", "shadow-lg"],
+    darkMode: "class",
+    theme: {
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+      extend: {
+        spacing: {
+          128: "32rem",
+          144: "36rem",
+        },
+        borderRadius: {
+          "4xl": "2rem",
+        },
+        maxWidth: {
+          "8xl": "90rem",
+          "9xl": "105rem",
+          "10xl": "120rem",
+        },
+        zIndex: {
+          1: 1,
+          60: 60,
+          70: 70,
+          80: 80,
+          90: 90,
+          100: 100,
+        },
+      },
+    },
+    variants: {
+      fill: [],
+      extend: {
+        borderColor: ["focus-visible"],
+        opacity: ["disabled"],
+      },
+    },
+    plugins: [require("flowbite/plugin")],
+}
